e8884f5c54595e2f1f86c66c5ba2a4fa397428e5
Helder M. V. R. <manuhelderruiz@gmail.com>
2024-10-17 17:01:10 +0200

    update: footer & img errors fixed

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ import "./style.css";
 import { createNavbar } from "./src/components/Header/Header";
 import { createAboutMe } from "./src/components/AboutMe/AboutMe";
 import { lighDarkBtn } from "./src/utils/changeMode";
+import { createFooter } from "./src/components/Footer/Footer";
 
 const init = () => {
   createNavbar();
   lighDarkBtn();
   createAboutMe();
+  createFooter();
 };
 
 init();
